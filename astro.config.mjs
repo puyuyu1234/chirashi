7b68110e9305f066a9453238a6f677824852b82f
ag5jm <ag5jm@proton.me>
2025-11-23 17:06:32 +0900

    [format]

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://puyuyu1234.github.io',
-	base: '/chirashi',
-	integrations: [mdx(), sitemap()],
+  site: 'https://puyuyu1234.github.io',
+  base: '/chirashi',
+  integrations: [mdx(), sitemap()],
 });
